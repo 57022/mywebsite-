ede6cd17682632cac8438f4de5ce39290a7ccfaf
57022 <97937658+57022@users.noreply.github.com>
2022-02-08 14:53:18 +0700

    Update table.php

--- table.php
+++ table.php
@@ -17,3 +17,10 @@
         <td>มาร์ค</td>
         <td><img src="https://pin.it/2uKcUxJ" width="130px"></td>
         <td><a href="https://instagram.com/onyourm__ark?utm_medium=copy_link">instagram</a></td>
+      <tr>
+        <td>1</td>
+        <td>แฮชาน ลี</td>
+        <td>แฮชาน</td>
+        <td><img src="" width="130px"></td>
+        <td><a href="https://instagram.com/fullsun_ncit?utm_medium=copy_link">instagram</a></td>
+
