a6d838aea026b86c08929906797b88456347ab5f
57022 <97937658+57022@users.noreply.github.com>
2022-02-08 14:30:10 +0700

    Update table.php

--- table.php
+++ table.php
@@ -8,11 +8,13 @@
         <th>ลำดับ</th>
         <th>ชื่อ - นามสกุล</th>
         <th>ชื่อเล่น</th>
+        <th>รูป</th>
       </tr>
       <tr>
         <td>1</td>
         <td>กชกร นามสิมมา</td>
         <td>มิ่งขวัญ</td>
+        <td><img src="https://images.app.goo.gl/KbaCgysEUMjSzNnw5" width="130px"</td>
       </tr>
     </table>
   </body> 
