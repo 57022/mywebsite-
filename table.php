1aa613ff678d9c9dda6dd78b98522127e9057187
57022 <97937658+57022@users.noreply.github.com>
2022-02-08 15:01:17 +0700

    Update table.php

--- table.php
+++ table.php
@@ -18,31 +18,31 @@
         <td><img src="" width="130px"></td>
         <td><a href="https://instagram.com/onyourm__ark?utm_medium=copy_link">instagram</a></td>
       <tr>
-        <td>1</td>
+        <td>2</td>
         <td>แฮชาน ลี</td>
         <td>แฮชาน</td>
         <td><img src="" width="130px"></td>
         <td><a href="https://instagram.com/fullsun_ncit?utm_medium=copy_link">instagram</a></td>
       <tr>
-        <td>1</td>
+        <td>3</td>
         <td>ยองโฮ ซอ</td>
         <td>จอห์นนี่</td>
         <td><img src="" width="130px"></td>
         <td><a href="https://instagram.com/johnnyjsuh?utm_medium=copy_link">instagram</a></td>
       <tr>
-        <td>1</td>
+        <td>4</td>
         <td>แทอิล มูน</td>
         <td>แทอิล</td>
         <td><img src="" width="130px"></td>
         <td><a href="https://instagram.com/mo.on_air?utm_medium=copy_link">instagram</a></td>
       <tr>
-        <td>1</td>
+        <td>5</td>
         <td>ยุนโอ จอง</td>
         <td>แจฮยอน</td>
         <td><img src="" width="130px"></td>
         <td><a href="https://instagram.com/_jeongjaehyun?utm_medium=copy_link">instagram</a></td>
       <tr>
-        <td>1</td>
+        <td>6</td>
         <td>ดงยอง คิม</td>
         <td>โดยอง</td>
         <td><img src="" width="130px"></td>
