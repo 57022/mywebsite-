563914722aee364863a65b2933dd1620980fed1c
57022 <97937658+57022@users.noreply.github.com>
2022-02-08 14:18:36 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <td>คอลัมที่ 1</td>
+        <td>คอลัมที่ 2</td>
+      </tr>
+    </table>
+  </body> 
+</html>
