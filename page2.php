17864f2bf74a0caa37f669a8d1a7a94bdc3e1cc3
57022 <97937658+57022@users.noreply.github.com>
2022-02-15 14:07:05 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -12,11 +12,13 @@
 			body{
                                 background-color: pink;
 			}
+			h1{
+				color: yellow;      
 		</style>
 	</head>
 	<body>
 		<div id="box1" class="box">
-			<h1>กล่องที่ 1</h1>
+			<h1>นางสาวกชกร นามสิมมา ชั้น 4/13 เลขที่ 13</h1>
 		</div>
 	</body>
 </html>
