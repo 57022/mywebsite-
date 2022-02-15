e2c8f826514fcc457dec7ef5c5c2691be3c2fd24
57022 <97937658+57022@users.noreply.github.com>
2022-02-15 13:35:41 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,17 +1,17 @@
 <html>
-	      <head>
-		            <title>การตกแต่งเว็บไซต์ด้วย CSS</title>
-		            <style>
-                                      h1{
-					      color: green;
-				      }
-                                      h2{
-                                              color: yellow;
-				      }
-		            </style>
-        </head>
-      	<body>
-                <h1>ข้อความนี้สีเขียว</h1>
-		<h2>ข้อความนี้สีเหลือง</h2>
-	      </body>
+	<head>
+		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#text1{
+				color:green;
+			}
+			#text2{
+				color:yellow;
+			}
+		</style>
+	</head>
+	<body>
+		<h1 id="text1"> ข้อความนี้สีเขียว H1 </h1>
+		<h1 id="text2"> ข้อความนี้สีเหลือง H1 </h1>
+	</body>
 </html>
